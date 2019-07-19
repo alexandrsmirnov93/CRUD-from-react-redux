f86451adf87ae7895304fddd5f00db77056389dd
alexandrsmirnov93 <alexandrsmirnov93@gmail.com>
2019-07-20 00:47:20 +0300

    +роутер

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 import thunk from 'redux-thunk';//На случай, если данные из json будут приходить из сервера
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 const store = createStore(rootReducers, applyMiddleware(thunk));
 
 
 
 ReactDOM.render(
   <Provider store={store}>
-     <App />
+    <BrowserRouter>
+          <Route path="/" component={App}/>
+     </BrowserRouter>
+    
   </Provider>
   ,
   document.getElementById('root')
